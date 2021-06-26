f30c907fc72030ce3bf4103727bdeba31c65f7c6
PelaNisha <nishapela777@gmail.com>
2021-06-26 16:36:40 +0545

    twelth commit

--- theblog/admin.py
+++ theblog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import post, Category
+from .models import post, Category, Profile
 # Register your models here.
 admin.site.register(post)
 admin.site.register(Category)
+admin.site.register(Profile)
--- theblog/models.py
+++ theblog/models.py
@@ -16,6 +16,13 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('home')
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null = True, on_delete = models.CASCADE)
+    bio =models.TextField()
+
+    def __str__(self):
+        return str(self.user)
+
 class post(models.Model):
     title = models.CharField(max_length = 255)
     header_image = models.ImageField(null = True, blank = True, upload_to = 'image/')
